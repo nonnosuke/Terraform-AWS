494a2731f6de4f0805a00c048cc8a54da999ccb8
nonnosuke <168043854+nonnosuke@users.noreply.github.com>
2025-10-29 21:38:42 -0700

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -60,7 +60,23 @@ resource "aws_instance" "permanent_agent" {
   subnet_id     = aws_subnet.main_b.id
   security_groups = [aws_security_group.web_sg.id]
   key_name        = var.key_name
-  user_data       = file("scripts/permanent_agent_setup.sh")
+  user_data       = <<-EOF
+                #!/bin/bash
+                # Update system
+                sudo dnf update -y
+
+                # Install required tools
+                sudo dnf install -y git java-21-amazon-corretto git
+
+                # Add 1 GB Swap
+                sudo fallocate -l 1G /swapfile_extend_1GB
+                sudo chmod 600 /swapfile_extend_1GB
+                sudo mkswap /swapfile_extend_1GB
+                sudo swapon /swapfile_extend_1GB
+
+                # Extend /tmp directory
+                sudo mount -o remount,size=5G /tmp/
+                EOF
   tags = {
     Name = "Permanent_Agent"
   }
